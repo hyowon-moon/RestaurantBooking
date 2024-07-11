d2f18abdc08be459bc39fa2026f22901ef74127d
hyowon.moon <hyowon.moon@samsung.com>
2024-07-11 10:57:06 +0900

    [Refactoring]

--- booking_scheduler_test.py
+++ booking_scheduler_test.py
@@ -5,32 +5,37 @@ from datetime import datetime
 from booking_scheduler import BookingScheduler
 from schedule import Customer, Schedule
 
+CAPACITY_PER_HOUR = 3
+
+UNDER_CAPCACITY = 1
+
+CUSTOMER = Customer("Fake Name", "010-1234-5678")
+NOT_ON_THE_HOUR = datetime.strptime("2021/03/26 09:05", "%Y/%m/%d %H:%M")
+ON_THE_HOUR = datetime.strptime("2021/03/26 09:00", "%Y/%m/%d %H:%M")
+
 
 class BookingSchedulerTest(unittest.TestCase):
 
+    def setUp(self):
+        self.booking_scheduler = BookingScheduler(CAPACITY_PER_HOUR)
+
     def test_예약은_정시에만_가능하다_정시가_아닌경우_예약불가(self):
         # arrange
-        not_on_the_hour = datetime.strptime("2021/03/26 09:05", "%Y/%m/%d %H:%M")
-        customer = Customer("Fake Name", "010-1234-5678")
-        schedule = Schedule(not_on_the_hour, 1, customer)
-        booking_scheduler = BookingScheduler(3)
+        schedule = Schedule(NOT_ON_THE_HOUR, UNDER_CAPCACITY, CUSTOMER)
 
         # act and assert
         with self.assertRaises(ValueError):
-            booking_scheduler.add_schedule(schedule)
+            self.booking_scheduler.add_schedule(schedule)
 
     def test_예약은_정시에만_가능하다_정시인_경우_예약가능(self):
         # arrange
-        on_the_hour = datetime.strptime("2021/03/26 09:00", "%Y/%m/%d %H:%M")
-        customer = Customer("Fake Name", "010-1234-5678")
-        schedule = Schedule(on_the_hour, 1, customer)
-        booking_scheduler = BookingScheduler(3)
+        schedule = Schedule(ON_THE_HOUR, UNDER_CAPCACITY, CUSTOMER)
 
         # act
-        booking_scheduler.add_schedule(schedule)
+        self.booking_scheduler.add_schedule(schedule)
 
         # assert
-        self.assertTrue(booking_scheduler.has_schedule(schedule))
+        self.assertTrue(self.booking_scheduler.has_schedule(schedule))
 
     def test_시간대별_인원제한이_있다_같은_시간대에_Capacity_초과할_경우_예외발생(self):
         pass
